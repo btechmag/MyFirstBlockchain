025a500944c2bc353787f358ff87b32a8c4d6efc
Nagaraj Gobbur <btechmag@gmail.com>
2021-08-23 09:42:32 +0530

    first commit

---
+++ src/com/keoch/simpleJavaBlockchain/TransactionInput.java
@@ -0,0 +1,11 @@
+package com.keoch.simpleJavaBlockchain;
+
+public class TransactionInput {
+	
+	public String transactionOutputId; //Reference to TransactionOutputs -> transactionId
+	public TransactionOutput UTXO; //contains the unspent transaction output
+	
+	public TransactionInput(String transactionOutputId) {
+		this.transactionOutputId = transactionOutputId;
+	}
+}
